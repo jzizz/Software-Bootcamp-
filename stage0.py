ddda3fafaa247d12ca2e10cd62dadfd75d23afaf
jzizz <79452697+jzizz@users.noreply.github.com>
2021-04-14 12:54:34 +0200

    Update stage0.py

--- stage0.py
+++ stage0.py
@@ -7,7 +7,7 @@ payload = None
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/json",
-    "X-Cisco-Meraki-API-Key": "6bec40cf957de430a6f1f2baa056b99a4fac9ea0"
+    "X-Cisco-Meraki-API-Key": ""
 }
 
 response = requests.request('GET', url, headers=headers, data = payload)
